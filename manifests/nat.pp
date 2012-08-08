69c2cdb1472012dc2247fb41cf6c4839d3a8380c
Adam Jahn <ajjahn@gmail.com>
2012-08-08 18:10:34 -0400

    crude nat implementation
    add commit

---
+++ manifests/nat.pp
@@ -0,0 +1,36 @@
+class ufw::nat {
+
+  #   augeas { "sysctl.conf":
+  #   context => '/files/etc/ufw/sysctl.conf',
+  #   changes => $ensure ? {
+  #     present => 'set net.ipv4.ip_forward 1',
+  #     default => 'rm net.ipv4.ip_forward',
+  #   }
+  # }
+
+  file { "/etc/ufw/sysctl.conf":
+    ensure => present,
+    owner => 'root',
+    group => 'root',
+    mode => 0644,
+    content => template("${module_name}/sysctl.conf.erb")
+  }
+
+  file { "/etc/ufw/before.rules":
+    ensure => present,
+    owner => 'root',
+    group => 'root',
+    mode => 0644,
+    content => template("${module_name}/before.rules.erb")
+  }
+	$ensure = present
+	augeas { "ufw-conf":
+		context => '/files/etc/default/ufw',
+		changes => $ensure ? {
+		  present => 'set DEFAULT_FORWARD_POLICY \'"ACCEPT"\'',
+		  default => 'set DEFAULT_FORWARD_POLICY \'"DROP"\'',
+		},
+		notify => Service['ufw']
+	}
+
+}
