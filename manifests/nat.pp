9079eb37e80f22ab16f9653f683b262f955b52f0
Adam Jahn <ajjahn@gmail.com>
2012-08-16 09:56:19 -0400

    specify iptable rules from site manifest

--- manifests/nat.pp
+++ manifests/nat.pp
@@ -1,35 +1,43 @@
-class ufw::nat {
+class ufw::nat($ensure = present, $filter_rules = false, $nat_rules = false) {
 
-  #   augeas { "sysctl.conf":
-  #   context => '/files/etc/ufw/sysctl.conf',
-  #   changes => $ensure ? {
-  #     present => 'set net.ipv4.ip_forward 1',
-  #     default => 'rm net.ipv4.ip_forward',
-  #   }
-  # }
+  augeas { "sysctl.conf":
+    context => '/files/etc/sysctl.conf',
+    changes => $ensure ? {
+      present => 'set net.ipv4.ip_forward 1',
+      default => 'rm net.ipv4.ip_forward',
+    },
+    require => Exec['ufw-default-deny'],
+    before  => Exec['ufw-enable'],
+  }
 
   file { "/etc/ufw/sysctl.conf":
     ensure => present,
     owner => 'root',
     group => 'root',
     mode => 0644,
-    content => template("${module_name}/sysctl.conf.erb")
+    content => template("${module_name}/sysctl.conf.erb"),
+    require => Exec['ufw-default-deny'],
+    before  => Exec['ufw-enable'],
   }
 
   file { "/etc/ufw/before.rules":
     ensure => present,
     owner => 'root',
     group => 'root',
-    mode => 0644,
-    content => template("${module_name}/before.rules.erb")
+    mode => 0600,
+    content => template("${module_name}/before.rules.erb"),
+    require => Exec['ufw-default-deny'],
+    before  => Exec['ufw-enable'],
   }
-	$ensure = present
-	augeas { "ufw-conf":
+
+	augeas { "ufw-manage-builtins-conf":
 		context => '/files/etc/default/ufw',
 		changes => $ensure ? {
-		  present => 'set DEFAULT_FORWARD_POLICY \'"ACCEPT"\'',
-		  default => 'set DEFAULT_FORWARD_POLICY \'"DROP"\'',
+		  present => 'set MANAGE_BUILTINS yes',
+		  default => 'set MANAGE_BUILTINS no',
 		},
+		require => Exec['ufw-default-deny'],
+		before  => Exec['ufw-enable'],
 	}
 
 }
