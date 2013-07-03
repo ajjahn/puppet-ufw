4213a692a49a6d5003a64e65dc5dd1abbff971cd
Adam Jahn <ajjahn@gmail.com>
2013-07-03 14:05:45 -0400

    support rhel distros with custom rpm

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,33 @@
 class ufw {
-  package { 'ufw':
-    ensure => present,
+
+  case $::osfamily {
+    debian: {
+      package { 'ufw':
+        ensure => present,
+      }
+    }
+    redhat: {
+      file { '/tmp/ufw-0.33_x86_64.rpm':
+        ensure => present,
+        source => "puppet:///modules/${module_name}/ufw-0.33_x86_64.rpm",
+      }
+      
+      package { 'ufw':
+        ensure => present,
+        provider => rpm,
+        source => '/tmp/ufw-0.33_x86_64.rpm',
+        require => File['/tmp/ufw-0.33_x86_64.rpm'],
+      }
+    }
   }
 
-  Package['ufw'] -> Exec['ufw-default-deny'] -> Exec['ufw-enable']
+
+  Package['ufw'] -> Exec['iptables-flush'] -> Exec['ufw-default-deny'] -> Exec['ufw-enable']
+
+  exec { 'iptables-flush':
+    command => 'iptables -F',
+    unless  => 'ufw status verbose | grep "Default: deny (incoming), allow (outgoing)"',
+  }
 
   exec { 'ufw-default-deny':
     command => 'ufw default deny',
